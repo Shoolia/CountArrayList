15c6718b8fb5612a8f0b93e518952550d61b3a32
Dima Shoolia <mrschulpov97@gmail.com>
2019-12-19 13:29:04 +0200

    1 commit create method for count number in ArrayList

--- src/main/java/ua/training/Main.java
+++ src/main/java/ua/training/Main.java
@@ -8,7 +8,7 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> arrayList = new ArrayList<Integer>(Arrays.asList(4,5,-6,4,5,3,4,2,4,5,7));
+        ArrayList<Integer> arrayList = new ArrayList<>(Arrays.asList(4,5,-6,4,5,3,4,2,4,5,7));
 
         arrayList.stream()
                 .collect(Collectors.groupingBy(
