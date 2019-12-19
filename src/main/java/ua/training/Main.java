78fd6de95ea8ceda054a61570d240a3768764012
Dima Shoolia <mrschulpov97@gmail.com>
2019-12-19 13:28:42 +0200

    1 commit create method for count number in ArrayList

---
+++ src/main/java/ua/training/Main.java
@@ -0,0 +1,21 @@
+package ua.training;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<Integer>(Arrays.asList(4,5,-6,4,5,3,4,2,4,5,7));
+
+        arrayList.stream()
+                .collect(Collectors.groupingBy(
+                        Integer::intValue,
+                        () -> new TreeMap<>(Comparator.comparingInt(Integer::intValue)),
+                        Collectors.counting()))
+                .forEach((a,b) -> System.out.println(a + " " + b ));
+        }
+    }
+
